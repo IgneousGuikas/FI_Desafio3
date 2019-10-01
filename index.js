4f5c552a5cd45a45d60540b6a80c423d93522c96
IgneousGuikas <rodrigo.rgk@hotmail.com>
2019-10-01 18:35:22 +0000

    Teste 3

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+var app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.get('/', (req,res)=>res.send("Consegui!"));
+
+app.listen(3000,()=>console.log("Listening to port 3000"));
