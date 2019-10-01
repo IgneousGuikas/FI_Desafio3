c0cf5a707b147f6dd637d2918e22d58817825470
IgneousGuikas <rodrigo.rgk@hotmail.com>
2019-10-01 19:40:25 +0000

    Testes da aplicação

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ var app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-app.get('/', (req,res)=>res.send("Consegui!"));
+app.get('/', (req,res)=>res.send("Consegui2!"));
 
 app.listen(3000,()=>console.log("Listening to port 3000"));
