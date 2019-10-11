edca18a2d42a93c17042bff38b6f1df614edf9e5
IgneousGuikas <rodrigo.rgk@hotmail.com>
2019-10-11 23:46:28 +0000

    Atualização página logData

--- logData/index.js
+++ logData/index.js
@@ -1,21 +1,20 @@
-function update(){
-  $.ajax({
-    method: "GET",
-    url: "http://18.223.194.18/getData"
-  }).done(function(data){
-    var temp = data.split("SP,SP");
-    var text = "";
-    var i;
-    for(i=0; i<temp.length; i++){
-      temp2 = JSON.parse(temp[i]);
-      text = text + "<p>Date: " + temp2.date + "</p>";
-      text = text + "<p>Dados: " + temp2.dados + "</p>";
-      text = text + "<p>Request: " + temp2.request + "</p>";
-    }
-    $('div').prepend(text);
-  })
-}
+$(document).ready(function() {
 
-$(document).ready(function(){
-  update();
-})
+  $("table").css("visibility", "visible");
+  $("#processT").hide();
+  $("#alarmsT").hide();
+  $("#activitiesT").hide();
+
+  $("select").change(function() {
+
+    var temp = "#" + $(this).children("option:selected").val() + "T";
+
+    $("#processT").hide();
+    $("#alarmsT").hide();
+    $("#activitiesT").hide();
+
+    $(temp).show();
+
+  });
+
+});
